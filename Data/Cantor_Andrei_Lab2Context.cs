8e213ee58c660d398bb10b9e2234656b395c758d
Cantor Andrei <cantorandrei18@gmail.com>
2023-10-22 20:57:31 +0300

    Add project files.

---
+++ Data/Cantor_Andrei_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Cantor_Andrei_Lab2.Models;
+
+namespace Cantor_Andrei_Lab2.Data
+{
+    public class Cantor_Andrei_Lab2Context : DbContext
+    {
+        public Cantor_Andrei_Lab2Context (DbContextOptions<Cantor_Andrei_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Cantor_Andrei_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Cantor_Andrei_Lab2.Models.Publisher>? Publisher { get; set; }
+    }
+}
