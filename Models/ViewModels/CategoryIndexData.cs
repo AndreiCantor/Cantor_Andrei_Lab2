3143c0050209311c8d7145835525a0f314390db3
Cantor Andrei <cantorandrei18@gmail.com>
2023-11-05 21:33:34 +0200

    rezolvare lab4

---
+++ Models/ViewModels/CategoryIndexData.cs
@@ -0,0 +1,8 @@
+﻿namespace Cantor_Andrei_Lab2.Models.ViewModels
+{
+    public class CategoryIndexData
+    {
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
